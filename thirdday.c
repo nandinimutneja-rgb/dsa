38ac4eef3a593d6eef8866385f59c158be4c794a
nandinimutneja-rgb <nandinimutneja@icloud.com>
2026-02-09 23:02:04 +0530

    Add files via upload

---
+++ thirdday.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n, key, found = 0;
+    int comparisons = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    for (int i = 0; i < n; i++) {
+        comparisons++;
+
+        if (arr[i] == key) {
+            printf("Element found at position %d\n", i + 1);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("Element not found\n");
+    }
+
+    printf("Number of comparisons: %d\n", comparisons);
+
+    return 0;
+}
