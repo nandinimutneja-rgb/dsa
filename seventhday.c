cbb6f4cbef80763ce810bb74ef15e7f34ded3d47
nandinimutneja-rgb <nandinimutneja@icloud.com>
2026-02-11 10:45:42 +0530

    Add files via upload

---
+++ seventhday.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int fib(int n) {
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    int n;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Invalid input");
+        return 0;
+    }
+
+    printf("Fibonacci number is: %d\n", fib(n));
+
+    return 0;
+}
+
